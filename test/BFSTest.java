7fecd9791a804b5d032f76f72ab6f9f8fb2076bb
arjanmc <arjanchakravarthy22@gmail.com>
2023-03-24 20:33:15 -0400

    asdf

--- test/BFSTest.java
+++ test/BFSTest.java
@@ -85,27 +85,21 @@ public class BFSTest {
         TravelController TC1 = new TravelController();
         TC1.load("data/cities1.csv", "data/transport1.csv");
         TravelGraph graph1 = TC1.getGraph();
-        List<Transport> path1 = new BFS<City, Transport>()
-                .getPath(graph1, graph1.getCityByName("Providence"),
+        List<Transport> path1 = new BFS<City, Transport>().getPath(graph1, graph1.getCityByName("Providence"),
                 graph1.getCityByName("New York City"));
         List<Transport> list1 = TC1.mostDirectRoute("Providence","New York City");
 
         Assert.assertEquals(path1.size(),2);
-        Assert.assertEquals(path1.get(1).toString(),
-                new Transport(new City("Boston"),
-                new City("New York City"),
-                        TransportType.PLANE, 267, 50).toString());
-        Assert.assertEquals(path1.get(0).getTarget().toString(),
-                new City("Boston").toString());
+        Assert.assertEquals(path1.get(1).toString(), new Transport(new City("Boston"),
+                new City("New York City"), TransportType.PLANE, 267, 50).toString());
+        Assert.assertEquals(path1.get(0).getTarget().toString(), new City("Boston").toString());
         Assert.assertEquals(list1.size(),2);
 
-        List<Transport> path2 = new BFS<City, Transport>().getPath(graph1,
-                graph1.getCityByName("New York City"),
+        List<Transport> path2 = new BFS<City, Transport>().getPath(graph1, graph1.getCityByName("New York City"),
                 graph1.getCityByName("Providence"));
         Assert.assertEquals(path2.size(),1);
 
-        List<Transport> path3 = new BFS<City, Transport>().getPath(graph1,
-                graph1.getCityByName("Boston"),
+        List<Transport> path3 = new BFS<City, Transport>().getPath(graph1, graph1.getCityByName("Boston"),
                 graph1.getCityByName("Boston"));
         Assert.assertEquals(path3.size(),0);
     }
@@ -115,21 +109,18 @@ public class BFSTest {
         TravelController TC2 = new TravelController();
         TC2.load("data/cities2.csv", "data/transport2.csv");
         TravelGraph graph2 = TC2.getGraph();
-        List<Transport> path2 = new BFS<City, Transport>().getPath(graph2,
-                graph2.getCityByName("Providence"),
+        List<Transport> path2 = new BFS<City, Transport>().getPath(graph2, graph2.getCityByName("Providence"),
                 graph2.getCityByName("Boston"));
 
         Assert.assertEquals(path2,new LinkedList<>());
 
-        List<Transport> path3 = new BFS<City, Transport>().getPath(graph2,
-                graph2.getCityByName("Boston"),
+        List<Transport> path3 = new BFS<City, Transport>().getPath(graph2, graph2.getCityByName("Boston"),
                 graph2.getCityByName("Providence"));
 
         Assert.assertEquals(100, path3.get(0).getPrice(), DELTA);
         Assert.assertEquals(1, path3.size());
 
-        List<Transport> path4 = new BFS<City, Transport>().getPath(graph2,
-                graph2.getCityByName("Boston"),
+        List<Transport> path4 = new BFS<City, Transport>().getPath(graph2, graph2.getCityByName("Boston"),
                 graph2.getCityByName("Chicago"));
         Assert.assertEquals(2, path4.size());
         Assert.assertEquals(path4.get(1).getTarget().toString(),"Chicago");
@@ -144,25 +135,21 @@ public class BFSTest {
         TravelController TC3 = new TravelController();
         TC3.load("data/cities3.csv", "data/transport3.csv");
         TravelGraph graph3 = TC3.getGraph();
-        List<Transport> path3 = new BFS<City, Transport>().getPath(graph3,
-                graph3.getCityByName("Hyderabad"),
+        List<Transport> path3 = new BFS<City, Transport>().getPath(graph3, graph3.getCityByName("Hyderabad"),
                 graph3.getCityByName("Chennai"));
-        List<Transport> list3 = TC3.mostDirectRoute("Hyderabad",
-                "Chennai");
+        List<Transport> list3 = TC3.mostDirectRoute("Hyderabad","Chennai");
 
         Assert.assertEquals(path3.size(),2);
-        Assert.assertEquals(path3.get(1).getSource().toString(),
-                new City("Bangalore").toString());
-        Assert.assertEquals(path3.get(0).toString(),
-                new Transport(new City("Hyderabad"),
-                new City("Bangalore"),TransportType.BUS,
-                        20, 60).toString());
+        Assert.assertEquals(path3.get(1).getSource().toString(), new City("Bangalore").toString());
+        Assert.assertEquals(path3.get(0).toString(), new Transport(new City("Hyderabad"),
+                new City("Bangalore"),TransportType.BUS, 20, 60).toString());
 
-        List<Transport> path4 = new BFS<City, Transport>().getPath(graph3,
-                graph3.getCityByName("Chennai"),
+        List<Transport> path4 = new BFS<City, Transport>().getPath(graph3, graph3.getCityByName("Chennai"),
                 graph3.getCityByName("Hyderabad"));
         Assert.assertEquals(path4.size(),1);
 
         Assert.assertEquals(list3.size(),2);
     }
+
+    // TODO: write more tests + make sure you test all the cases in your testing plan!
 }
